36e85adea4e12172a520547876ffe785020b7fe2
jiahong <jiahonglin17@gmail.com>
2014-04-02 21:56:22 +0800

    modify target qemudbg in mk/qemu.mk for running gdb

--- mk/qemu.mk
+++ mk/qemu.mk
@@ -9,7 +9,8 @@ qemudbg: $(OUTDIR)/$(TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
 		-monitor stdio \
 		-gdb tcp::3333 -S \
-		-kernel $(OUTDIR)/$(TARGET).bin 2>&1>/dev/null & \
+		-kernel $(OUTDIR)/$(TARGET).bin
+		-kernel $(OUTDIR)/$(TARGET).bin 2>&1>/dev/null
 	echo $$! > $(OUTDIR)/qemu_pid && \
 	$(CROSS_COMPILE)gdb -x $(TOOLDIR)/gdbscript && \
 	cat $(OUTDIR)/qemu_pid | `xargs kill 2>/dev/null || test true` && \
